0456764bb18a68957a6bcc116864c80f81a0511c
Sergey Solodyagin <solodyagin@gmail.com>
2025-03-03 11:46:24 +0300

    Deployment to GitHub Pages

---
+++ vite.config-demo.ts
@@ -0,0 +1,18 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: '/vue3-device-detect/',
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    outDir: 'demo',
+  },
+})
