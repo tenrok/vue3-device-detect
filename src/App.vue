91e9b05273624e6ed2f0f167da42c68fc7d02dfb
Sergey Solodyagin <solodyagin@gmail.com>
2025-03-03 10:52:26 +0300

    Add files

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { deviceType, deviceModel, deviceVendor, osName, osVersion, browserName, browserVersion, engineName, engineVersion } from '../lib'
+</script>
+
+<template>
+  <ul>
+    <li>Device Type: {{ deviceType() }}</li>
+    <li>Device: {{ deviceVendor() }} {{ deviceModel() }}</li>
+    <li>OS: {{ osName() }} {{ osVersion() }}</li>
+    <li>Browser: {{ browserName() }} {{ browserVersion() }}</li>
+    <li>Engine: {{ engineName() }} {{ engineVersion() }}</li>
+  </ul>
+
+  <DesktopView>
+    <h1>This is rendered only in desktop browser</h1>
+  </DesktopView>
+
+  <MobileView>
+    <h1>This is rendered only on mobile</h1>
+  </MobileView>
+</template>
---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    lib: {
+      entry: 'lib/index.ts',
+      name: 'vue3-device-detect',
+      fileName: 'vue3-device-detect',
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+})
